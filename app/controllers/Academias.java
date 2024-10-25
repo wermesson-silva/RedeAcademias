43b41d8f1be68cbcce14e62ead1e14889ad38316
WERMESSON CLEINALDO BARBOSA DA SILVA <20211064010008@ifrn.local>
2024-10-25 13:50:14 -0300

    Adição do controlador de Academias

---
+++ app/controllers/Academias.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.util.List;
+
+import models.Academia;
+import models.Cliente;
+import models.Personal;
+import play.mvc.Controller;
+
+public class Academias extends Controller {
+	
+	public static void form() {
+		render();
+	}
+	
+	public static void salvar(Academia a) {
+		
+		String mensagem = "Academia cadastrada com sucesso!";
+		
+		if(a.id != null) {
+			mensagem = "Dados da Academia editados com sucesso!";
+		}
+		
+		a.save();
+		flash.success(mensagem);
+		listar(null);
+	}
+	
+	public static void listar(String termo) {
+		List<Academia> academias = null;
+		
+		if(termo == null) {
+			academias = Academia.findAll();			
+		} else {
+			academias =  Academia.find("lower(nome) like ?1 or cpf like ?1 or lower(nomeAcademia) like ?1", "%" + termo.toLowerCase() + "%").fetch();
+		}
+		render(academias, termo);
+	}
+	
+	public static void remover(Long id) {
+		Academia a = Academia.findById(id);
+		a.delete();
+		flash.success("Academia removida com sucesso!");
+		listar(null);
+	}
+	
+	public static void editar(Long id) {
+		
+		Academia academia = Academia.findById(id);
+		
+		renderTemplate("Clientes/form.html", academia);
+	}
+	
+}
